d698ffc6d92bae0a00646bbedc6d4bf71ac302f0
William Petty <william@snakeweed.com>
2018-12-03 08:23:57 -0700

    Goodenuff commit

--- app/controllers/symphonies_app.rb
+++ app/controllers/symphonies_app.rb
@@ -4,11 +4,20 @@ class SymphoniesApp < Sinatra::Base
 
   get '/composers' do
     if params[:age]
-      @composers = Composer.get_by_age(params[:age])
-    elsif params[:id]
-      @composers = Composer.get_by_id(params[:id])
+      @composers = Composer.where(age: params[:age])
+      @cities = @composers.distinct.order(:city).pluck(:city)
+    elsif params[:sort]
+      if params[:sort] == "name"
+        @composers = Composer.order(:surname)
+      elsif params[:sort] == "dod"
+        @composers = Composer.order(:dod)
+      elsif params[:sort] == "dob"
+        @composers = Composer.order(:dob)
+      end
+      @cities = Composer.distinct.order(:city).pluck(:city)
     else
-      @composers = Composer.all
+      @composers = Composer.order(:dob)
+      @cities = Composer.distinct.order(:city).pluck(:city)
     end
     erb :"composers/index"
   end
